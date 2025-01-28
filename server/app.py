20ca5aae3b08339b5b47625ddc84d5e3618e9831
Debrah Navajjah Muinde <debbynav645@gmail.com>
2025-01-28 23:53:17 +0300

    Create server and resources folder

---
+++ server/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_restful import Api
+from config import db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+migrate = Migrate(app, db)
+api = Api(app)
+
+
+CORS(app)
+
---
+++ server/models.py
@@ -0,0 +1,30 @@
+from config import db
+from sqlalchemy_serializer import SerializerMixin
+
+class Book(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    rarity = db.Column(db.String, nullable=False)
+    spell_type = db.Column(db.String, nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    hogwarts_class = db.Column(db.String, nullable=False)
+    reviews = db.relationship('Review', backref='book', cascade='all, delete-orphan')
+
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.rarity}', '{self.spell_type}', '{self.author}','{self.hogwarts_class}')"
+    
+class Review(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
+
+favorites = db.Table(
+    'favorites',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('book_id', db.Integer, db.ForeignKey('book.id'), primary_key=True)
+)
+class User(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    favorites = db.relationship('Book', secondary=favorites, backref='liked_by')
